0c951828238c02c1b5995d4760991c8621fc0a98
Master <1196532362@qq.com>
2024-10-11 14:48:24 +0800

    m

--- online/local/llm_api_redis.py
+++ online/local/llm_api_redis.py
@@ -1,36 +1,24 @@
 import redis
 import pickle
-import torch.multiprocessing as mp
 import argparse
-import torch
+import os
 from vllm import LLM, SamplingParams
 
-device = 'cuda:0'
-dir_list = [
-    "/home/jiangpeiwen2/jiangpeiwen2/workspace/LLMs/ChatGLM3-6B/ZhipuAI/chatglm3-6b",
-    "/home/jiangpeiwen2/jiangpeiwen2/workspace/LLMs/TKGT_Model/e2e_3_50/ZhipuAI/chatglm3-6b",
-    "/home/jiangpeiwen2/jiangpeiwen2/workspace/LLMs/TKGT_Model/e2e_3_50/3",
-    "/home/jiangpeiwen2/jiangpeiwen2/workspace/LLMs/TKGT_Model/e2e_3_50/4",
-    "/home/jiangpeiwen2/jiangpeiwen2/workspace/LLMs/TKGT_Model/e2e_3_50/5",
-    "/home/jiangpeiwen2/jiangpeiwen2/workspace/LLMs/TKGT_Model/e2e_3_50/6",
-]
-
-model_dir = dir_list[5]
-
-def Inference():
+def Inference( model_path, gpu_id ):
     redis_communication = redis.StrictRedis(host='localhost', port=6379, db=0)
     redis_communication.delete('Tasks')
     redis_communication.delete('Response')
-    sampling_params = SamplingParams(temperature=0, top_p=1, use_beam_search=True, best_of=2)
+    # sampling_params = SamplingParams(temperature=0, top_p=1, use_beam_search=True, best_of=2)
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
     llm = LLM(
-        model=model_dir,
+        model=model_path,
         trust_remote_code=True,
         dtype='float16',
         gpu_memory_utilization=0.7,
         seed = 32,
-        #device='cuda:2'
-    )   # 必须使用模型支持列表中的模型名
-    print(f"LLM Inderence start!")
+    )
+    print(f"LLM Inderence start at GPU {gpu_id} and you can connect it using {redis.StrictRedis(host='localhost', port=6379, db=0)}!")
     while True:
         message = redis_communication.lpop('Tasks')
         if message!=None:
@@ -42,4 +30,16 @@ def Inference():
             redis_communication.rpush('Response', pickle.dumps( (index, ret) ))
 
 if __name__=="__main__":
-    Inference( )
+    """
+    parser = argparse.ArgumentParser(description="Run script with external arguments")
+    parser.add_argument('--model_path', type=str, required=True, help='使用的模型路径')
+    parser.add_argument('--gpu_id', type=int, required=True, help='使用的GPU')
+    args = parser.parse_args()
+    # 获取参数值
+    
+    model_path = args.model_path
+    gpu_id = args.gpu_id
+    """
+    model_path = "/home/jiangpeiwen2/.cache/modelscope/hub/ZhipuAI/chatglm3-6b"
+    gpu_id = 1
+    Inference( model_path, gpu_id )
